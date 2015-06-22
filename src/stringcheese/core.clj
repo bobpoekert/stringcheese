0cef44d8b9a976491d30ab6fc4613e159232f107
Bob Poekert <bob@poekert.com>
2015-06-21 19:31:05 -0700

    fix instaparse parser

--- src/stringcheese/core.clj
+++ src/stringcheese/core.clj
@@ -5,62 +5,53 @@
 
 (def template-parser
   (insta/parser
-    "EXPR = (EXPR_TAG | EXPR_LITERAL)+
-     EXPR_TAG = '{%' EXPR '%}'
-     EXPR_LITERAL = #'(?:(?!(\\{%)|(%\\})).)+'"))
+    "EXPR = (EXPR_TAG | LITERAL)+
+     EXPR_TAG = '{%' EXPR_INSIDE '%}'
+     LITERAL = #'(?:(?!(\\{%)|(%\\})|(%\\{)|(\\}%)).)+'
+     EXPR_INSIDE = (LITERAL | UNEXPR)+
+     UNEXPR = '%{' UNEXPR_INSIDE '}%'
+     UNEXPR_INSIDE = (LITERAL | EXPR)+")) 
 
+(declare inner-compile-template)
 
-;; template-parser
+(defn gen-unexpr
+  [args]
+  (let [writer-sym (gensym "writer")]
+    `(let [^stringcheese.ArrayListWriter ~writer-sym (stringcheese.ArrayListWriter.)]
+      ~@(map (partial inner-compile-template writer-sym) args)
+      (.getContent ~writer-sym))))
 
-(defn parse-template
-  [template-string]
-  (let [pieces (re-seq expr-re template-string)]
-    (for [piece pieces]
-      (cond
-        (= (count piece) 1) {:type :string :value (first piece)}
-        (.startsWith (second piece) "*") {:type :meta :value (read-string (.substring (second piece) 1 -1))}
-        :else {:type :expr :value (read-string (second piece))}))))
+(defn gen-expr-inside
+  [[tag & args]]
+  (case tag
+    :LITERAL (first args)
+    :UNEXPR (recur (second args))
+    :UNEXPR_INSIDE (prn-str (gen-unexpr args))))
 
-(defn extract-meta
-  [template-results]
-  (let [[_meta body] (group-by #(= (:type %) :meta) template-results)
-        docstring (or (first (map string? _meta)) nil)
-        arglist (or (first (map vector? _meta)) nil)]
-    {:arglist arglist
-     :docstring docstring
-     :body body}))
-
-;;TODO
-(defn template-error
-  [ex-sym fragment]
-  `(stringcheese.TemplateException.
-      ~ex-sym ~(:line-number fragment) ~(:char-number fragment) ~(:source fragment)))
+(defn inner-compile-template
+  [writer-sym [tag & args]]
+  (case tag
+    :EXPR (cons 'do (map (partial inner-compile-template writer-sym) args))
+    :LITERAL `(.write ~writer-sym ~(first args))
+    :EXPR_TAG (recur writer-sym (second args))
+    :EXPR_INSIDE `(stringcheese.core/write-value!
+                  ~(read-string (apply str (map gen-expr-inside args)))
+                  ~writer-sym)))
 
 (defprotocol WriteValue
+  (write-value! [v target]))
+
+(extend-protocol WriteValue
   java.util.List
   (write-value! 
     [v ^java.io.Writer target]
-    (let [^java.util.Iteartor it (.iterator v)]
+    (let [^java.util.Iterator it (.iterator v)]
       (while (.hasNext it)
-        (write-value! (.next it)))))
+        (write-value! (.next it) target))))
   Object
   (write-value!
     [v ^java.io.Writer target]
     (.write target (str v))))
-
-(defn generate-funciton
-  [function-name {:keys [arglist docstring body]}]
-  (let [writer-sym (with-meta (gensym "writer") {:tag 'java.io.Writer})
-        ex-sym (gensym exception)]
-    `(defn ~function-name
-      ~docstring
-      ~(into [writer-sym] arglist)
-      ~@(for [fragment body]
-        (case (:type fragment)
-          :value `(.write ~writer-sym ~(:value fragment))
-          :expr `(try
-                  (stringcheese.core/write-value! ~(:value fragment) ~writer-sym)
-                  (catch Exception ~ex-sym (throw ~@(template-error ex-sym fragment)))))))))
 
 (defn render-string
   [thunk & args]
